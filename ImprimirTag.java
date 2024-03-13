09e71ca8fc703e1691fb11686e63b6c7fd566a74
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-03-13 18:55:30 +0100

    Update ImprimirTag.java

--- ImprimirTag.java
+++ ImprimirTag.java
@@ -1,1 +1,33 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Cargar el archivo HTML
+            File input = new File("C:\\Users\\ignac\\Desktop\\web.html"); //poned vuestra ruta
+            Document doc = Jsoup.parse(input, "UTF-8", "");
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Introduce el tag que deseas buscar");
+            String tag = entrada.nextLine();
+            mostrarTag(doc,tag);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void mostrarTag(Document doc, String tag) {
+
+        Elements encapçalaments = doc.getElementsByTag(tag);
+        for (Element encapçalament : encapçalaments) {
+            System.out.println(encapçalament.text());
+        }
+
+    }
+}
